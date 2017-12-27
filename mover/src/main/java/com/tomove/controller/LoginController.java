ae95c4f4d4dd1852af284ca4534011890769af95
Mikhail Nazarov <mcfoton@gmail.com>
2017-12-27 18:45:41 +0200

    added reset password flow, added beans

--- mover/src/main/java/com/tomove/controller/LoginController.java
+++ mover/src/main/java/com/tomove/controller/LoginController.java
@@ -4,19 +4,25 @@ package com.tomove.controller;
 import com.tomove.controller.to.DataTo;
 import com.tomove.model.subjectMover.Account;
 import com.tomove.repository.AccountRepository;
+import com.tomove.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
+import java.util.UUID;
 
-import static com.tomove.controller.PathConstant.GET_ALL_ACCOUNTS;
-import static com.tomove.controller.PathConstant.POST_LOGIN;
+import static com.tomove.controller.PathConstant.*;
 
 @RestController
 @CrossOrigin
 public class LoginController {
 
     private AccountRepository repository;
+
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
     public LoginController(AccountRepository repository) {
@@ -34,5 +40,56 @@ public class LoginController {
     public DataTo getAll() {
         Iterable<Account> account = repository.findAll();
         return account == null ? new DataTo(false, "Wrong login") : new DataTo(true, account);
+    }
+
+    @RequestMapping(value = FORGOT_PASSWORD, method = RequestMethod.POST)
+    public DataTo sendForgotPasswordEmail(@RequestBody Map<String, String> params) {
+        String email = params.get("email");
+        Account account = repository.findByEmail(email);
+        if (account == null) {
+            return new DataTo(false, String.format("No user with email: %s", email));
+        } else {
+            account.setVerificationCode(UUID.randomUUID().toString());
+            account.setFlPasswordUpdate(true);
+            repository.save(account);
+            sendResetPasswordEmail(email, account.getVerificationCode());
+            return new DataTo(true, String.format("Email was sent to %s", email));
+        }
+    }
+
+    private void sendResetPasswordEmail(String email, String token) {
+        // Email message
+        SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+        passwordResetEmail.setFrom("support@demo.com");
+        passwordResetEmail.setTo(email);
+        passwordResetEmail.setSubject("Password Reset Request");
+        passwordResetEmail.setText("To reset your password, click the link below:\n" + URL
+                + "/reset?token=" + token);
+
+        emailService.sendEmail(passwordResetEmail);
+    }
+
+    @RequestMapping(value = CHECK_TOKEN, method = RequestMethod.GET)
+    public DataTo checkToken(String token) {
+        Account account = repository.findByVerificationCode(token);
+        if (account == null) {
+            return new DataTo(false, "Password reset token invalid");
+        } else {
+            return new DataTo(true, "Password reset token valid");
+        }
+    }
+
+    @RequestMapping(value = RESET_PASSWORD, method = RequestMethod.POST)
+    public DataTo storeNewPassword(@RequestBody Map<String, String> params) {
+        String token = params.get("token");
+        Account account = repository.findByVerificationCode(token);
+        if (account == null || !account.isFlPasswordUpdate()) {
+            return new DataTo(false, "No such token"); // shouldn't happen, but we check anyway
+        } else {
+            account.setPassword(params.get("password"));
+            account.setFlPasswordUpdate(false);
+            repository.save(account);
+            return new DataTo(true, "Password saved");
+        }
     }
 }
--- mover/src/main/java/com/tomove/controller/PathConstant.java
+++ mover/src/main/java/com/tomove/controller/PathConstant.java
@@ -3,6 +3,7 @@ package com.tomove.controller;
 
 public class PathConstant {
 
+    public static final String URL = "http://localhost:8080";
     public static final String GET_ALL_ACCOUNTS = "/";
     public static final String POST_LOGIN = "/login";
     public static final String GET_RECENT_CUSTOMER_REQUESTS = "/dashboard/requests/customer/getrecent";
@@ -13,4 +14,9 @@ public class PathConstant {
     public static final String GET_DATE_MOVER_REQUESTS = "/dashboard/requests/mover/getfordate";
     
     public static final String REQUEST_DATE = "request_date";
+
+    public static final String FORGOT_PASSWORD = "/forgot";
+    public static final String RESET_PASSWORD = "/reset";
+    public static final String CHECK_TOKEN = "/token/check";
+
 }
