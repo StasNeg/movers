e8adb4d401f1e6e8e9066d8e92bd2c93de429f77
Eli Pocherk <lonsdale86@gmail.com>
2017-12-28 12:56:57 +0200

    Added URL string constants for Customer Dashboard

--- mover/src/main/java/com/tomove/controller/PathConstant.java
+++ mover/src/main/java/com/tomove/controller/PathConstant.java
@@ -5,4 +5,7 @@ public class PathConstant {
 
     public static final String GET_ALL_ACCOUNTS = "/";
     public static final String POST_LOGIN = "/login";
+    public static final String GET_RECENT_REQUESTS = "/dashboard/requests/getrecent";
+    public static final String GET_CALENDAR_REQUESTS = "/dashboard/requests/getforcalendar";
+    public static final String GET_DATE_REQUESTS = "/dashboard/requests/getfordate";
 }
