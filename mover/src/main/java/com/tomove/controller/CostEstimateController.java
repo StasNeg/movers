a5551cddd16f9527111c4ab8522a3541ad3f0ee3
Mikhail Nazarov <mcfoton@gmail.com>
2018-01-13 14:58:34 +0200

    WIP getting distance from Google Directions API

--- mover/src/main/java/com/tomove/controller/CostEstimateController.java
+++ mover/src/main/java/com/tomove/controller/CostEstimateController.java
@@ -1,5 +1,8 @@
 package com.tomove.controller;
 
+import com.google.maps.*;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
 import com.tomove.common.DataTo;
 import com.tomove.common.RequestData;
 import com.tomove.common.TotalCostEstimate;
@@ -9,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-
 import static com.tomove.common.PathConstant.*;
 
 @RestController
@@ -19,6 +20,8 @@ public class CostEstimateController {
 
     private AccountRepository accountRepository;
     private RequestRepository requestRepository;
+
+    private static double PRICE_PER_KM = 30;
 
     @Value("${cartonPrice}")
     Integer cartonPrice;
@@ -34,14 +37,36 @@ public class CostEstimateController {
 
     @PostMapping(value = GET_TOTAL_COST_ESTIMATE)
     public DataTo getTotalCostEstimate(@RequestBody RequestData data) {
-        System.out.println(data);
-//        Integer totalCost = calculateCost(data);
-        Integer totalCost = 200;
+        Integer totalCost = calculateCost(data);
         return new DataTo(true, new TotalCostEstimate(totalCost, cartonPrice, packetPrice));
     }
 
-    private Integer calculateCost(Map<String, String> data) {
-//        System.out.println(data);
-        return 200;
+    private Integer calculateCost(RequestData data) {
+        Double distancePrice = getDistance(data.getItems().get(0).getAddressIn(), data.getItems().get(0).getAddressOut())*PRICE_PER_KM;
+        //TODO CALCULATE PRICE FOR ALL ITEMS AND LIFTS
+        return distancePrice.intValue();
+    }
+
+    private Integer getDistance(String addressIn, String addressOut) {
+
+        String API_KEY = "AIzaSyAz9zX-L__RluZ8C3hgBWVE3YSoiprfuYs";
+
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(API_KEY)
+                .build();
+        DirectionsResult results = new DirectionsResult();
+        try {
+            results = new DirectionsApiRequest(context)
+                    .origin(addressIn)
+                    .destination(addressOut)
+                    .mode(TravelMode.DRIVING)
+                    .await();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return ((int) results.routes[0].legs[0].distance.inMeters) / 1000;
     }
 }
+
+
