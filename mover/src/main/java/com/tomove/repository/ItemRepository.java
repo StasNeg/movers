7f63fcbe6dc16bc6321f75f48767c552f0866be9
Mikhail Nazarov <mcfoton@gmail.com>
2018-01-31 14:44:34 +0200

    new methods in repositories

--- mover/src/main/java/com/tomove/repository/ItemRepository.java
+++ mover/src/main/java/com/tomove/repository/ItemRepository.java
@@ -4,9 +4,11 @@ import com.tomove.model.objectMover.Item;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 
 @Repository
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 
+    Optional<Item> findByName(String name);
 }
--- mover/src/main/java/com/tomove/repository/ItemTypeRepository.java
+++ mover/src/main/java/com/tomove/repository/ItemTypeRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemTypeRepository extends CrudRepository< ItemType, Integer> {
 
-
 	Iterable<ItemType> findAllByroomType(RoomType roomType);
+	Optional<ItemType> findByName(String name);
 
 	@Query("select i.properties from ItemType i where i.id = :idItemType" )
 	Iterable<TypeProperties> findTypeProperties(@Param("idItemType") Integer idItemType);
