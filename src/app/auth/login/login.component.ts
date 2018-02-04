652363693b9a2a07eedbef58cde50a7d65b7df68
oleg sipiagin <fed4wet@hotmail.com>
2018-02-04 15:37:35 +0200

    Add Login Form

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {UsersService} from '../../services/users.service';
+import {AuthService} from '../../services/auth.service';
+import {Message} from '../models/message.model';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  message: Message;
+
+  constructor(private usersService: UsersService,
+              private authService: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.message = new Message('danger', '');
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['nowCanLogin']) {
+        this.showMessage({text: 'Enter in system', type: 'success'});
+      }
+    });
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  private showMessage(message: Message) {
+    this.message = message;
+
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
+  onSubmit() {
+    const formData = this.form.value;
+    this.usersService.getUserByEmail(formData.email, formData.password).subscribe((data: Object)=>{console.log(data)});
+
+  }
+}
