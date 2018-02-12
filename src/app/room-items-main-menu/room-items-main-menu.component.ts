467e9641d86e58168a1436f2cdfb50bb7828cb1b
Stanislav <stasn@ua.fm>
2018-02-12 20:16:02 +0200

    Finish with itemsInput

--- src/app/room-items-main-menu/room-items-main-menu.component.ts
+++ src/app/room-items-main-menu/room-items-main-menu.component.ts
@@ -5,7 +5,6 @@ import {MatDialog} from '@angular/material';
 import {DialogComponent} from './dialog/dialogItem/dialog.component';
 import {DialogRoomComponent} from './dialog/dialogRoom/dialogRoom.component';
 import {Room} from '../interfaces/room';
-import {Mover} from '../interfaces/mover';
 
 
 @Component({
@@ -14,30 +13,45 @@ import {Mover} from '../interfaces/mover';
   styleUrls: ['./room-items-main-menu.component.css']
 })
 export class RoomItemsMainMenuComponent implements OnInit {
-
+  //array for color (addresses and items)
   color = ['yellow', 'blue', 'pink', 'green', 'purple', 'orange', 'brown', 'dark-blue'];
+  //main array type:ItemAddressData (watch into interface)
   arrayItems: ItemAddressData [] = [];
   roomType;
   // array with addresses
   addresses;
+  // array with items which are displayed in part TO on every address
   itemsTo;
+  // array with items which are displayed in part FROM on every address
   items = [];
+  // array with type "0_0_0 : 0"
+  // where key (all from arrayItems):
+  // first 0 - number in address array,
+  // second 0 - number in room array,
+  // third 0 - number in itemsArray,
+  //value: number in address which represent address TO go that item
   itemsToIndex = [];
+  //current highlighted address
   addressesCurrentIndex = -1;
+  //current highlighted room
   roomTypeCurrentIndex = -1;
+  //variable for close all modal forms
   open = false;
+  //variable for roomType array
   roomTypeTotal;
-  mover: Mover;
+  //variable for init addresses From localStorage
   addressesLocalStorage;
 
   constructor(private itemService: ItemService, public dialog: MatDialog) {
   }
 
   ngOnInit() {
+    //init addresses
     this.addressesLocalStorage = JSON.parse(localStorage.getItem('adresses'));
     this.addresses = [];
     this.addresses.push(this.addressesLocalStorage.addressFrom);
     this.addressesLocalStorage.addressesTo.forEach((i) => this.addresses.push(i));
+    //get roomType from server, init arrayItems
     this.itemService.getAppartmentsType().subscribe((responce) => {
       this.roomTypeTotal = [];
       this.roomTypeTotal.push(...responce.data);
@@ -45,6 +59,7 @@ export class RoomItemsMainMenuComponent implements OnInit {
     });
   }
 
+  //init arrayItems Method
   createArrayItems() {
     for (let i = 0; i < this.addresses.length; i++) {
       const tempRooms: Room[] = [];
@@ -64,21 +79,28 @@ export class RoomItemsMainMenuComponent implements OnInit {
     }
   }
 
+  //get color for address
   getColor(i) {
     return this.color[i];
   }
-  getColorTo(){
+
+  //get color for TO items
+  getColorTo() {
     return this.color[this.addressesCurrentIndex];
   }
 
-  getColorFrom(i){
+  //get color for FROM items
+  getColorFrom(i) {
     return this.color[this.itemsToIndex[this.createIndexForItemsTo(i)]];
   }
+
+  //onClick onRoomChange
   onRoomTypeChange(i) {
     this.roomTypeCurrentIndex = i;
     this.showItems();
   }
 
+  //onClick onAddressChange
   onAddressChange(i) {
     this.addressesCurrentIndex = i;
     this.roomType = [];
@@ -94,6 +116,7 @@ export class RoomItemsMainMenuComponent implements OnInit {
     this.showItems();
   }
 
+  //init items array for display items From
   showItems() {
     if (!this.canAddItem()) {
       return;
@@ -108,6 +131,7 @@ export class RoomItemsMainMenuComponent implements OnInit {
     }
   }
 
+  //methods for work with dialog window
   openRoomDialog() {
     this.open = true;
     this.dialog.closeAll();
@@ -132,7 +156,7 @@ export class RoomItemsMainMenuComponent implements OnInit {
     });
   }
 
-  openDialog(): void {
+  openItemDialog(): void {
     this.open = true;
     this.dialog.closeAll();
     let dialogRef = this.dialog.open(DialogComponent, {
@@ -157,7 +181,7 @@ export class RoomItemsMainMenuComponent implements OnInit {
       }
     });
   }
-
+  //init array properties in items
   private arrayFrom(property) {
     let result = [];
     for (let i in  property) {
@@ -166,26 +190,30 @@ export class RoomItemsMainMenuComponent implements OnInit {
     return result;
   }
 
+  //validation address
   isSelectedAddress() {
     return this.addressesCurrentIndex >= 0 && this.addressesCurrentIndex !== this.addresses.length - 1;
   }
 
+  //validation add item
   canAddItem() {
     if (this.addressesCurrentIndex < 0 || this.roomTypeCurrentIndex < 0 || this.addressesCurrentIndex === this.addresses.length - 1) {
       return false;
     }
     return true;
   }
-
+  //create index for itemsToIndex
   createIndexForItemsTo(indexItem) {
     return (this.addressesCurrentIndex + '_' + this.roomTypeCurrentIndex + '_' + indexItem);
   }
 
+  //get item for itemsTo array
   getItemByIndexItemTo(index: String) {
     let indexes = index.split('_');
     return this.arrayItems[+indexes[0]].rooms[+indexes[1]].items[+indexes[2]];
   }
 
+  //create itemTo for current address
   private getAllItemsTo() {
     let items = [];
     for (var key in this.itemsToIndex) {
@@ -195,13 +223,31 @@ export class RoomItemsMainMenuComponent implements OnInit {
     }
     return items;
   }
-  changeAddressTo(itemIndex ,addressToIndex){
+
+  //change destinations for item - itemIndex, destination - addressToIndex
+  changeAddressTo(itemIndex, addressToIndex) {
     if (addressToIndex > this.addressesCurrentIndex) {
       this.itemsToIndex[this.createIndexForItemsTo(itemIndex)] = addressToIndex;
-      this.showItems();
     }
   }
-  deleteItem(i){
-    console.log(i);
+
+  //remove item from arrayItems, rearrange itemToIndexArray
+  deleteItem(itemIndex) {
+    let index = this.createIndexForItemsTo(itemIndex);
+    let tempArray = []
+    this.arrayItems[this.addressesCurrentIndex].rooms[this.roomTypeCurrentIndex].items.splice(itemIndex, 1);
+    for (var key in this.itemsToIndex) {
+      if (index !== key) {
+        let newKey;
+        let splitArray = key.split('_')
+        if (+splitArray[0] === this.addressesCurrentIndex && +splitArray[1] === this.roomTypeCurrentIndex && +splitArray[2] > itemIndex) {
+          newKey = this.createIndexForItemsTo(+splitArray[2] - 1)
+        } else {
+          newKey = key;
+        }
+        tempArray[newKey] = this.itemsToIndex[key];
+      }
+    }
+    this.itemsToIndex = tempArray;
   }
 }
